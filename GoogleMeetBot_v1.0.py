8f5bd6d324688aeed06f529de976c6dd85d78075
SuyeshBadge <59205766+SuyeshBadge@users.noreply.github.com>
2020-05-12 00:08:36 +0530

    Create GoogleMeetBot_v1.0.py

---
+++ GoogleMeetBot_v1.0.py
@@ -0,0 +1,7 @@
+Things need to set up the bot
+
+python3 v3.0.6 or above                                
+1>Selenium webdriver                                 
+python -m pip install selenium                            
+2>pyautoGui                                 
+python -m pip install pyautogui                         
