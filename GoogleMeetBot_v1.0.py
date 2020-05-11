59bf50ea7acb9b28943699a72530f5c5f0401dcb
SuyeshBadge <59205766+SuyeshBadge@users.noreply.github.com>
2020-05-12 00:10:53 +0530

    Update GoogleMeetBot_v1.0.py

--- GoogleMeetBot_v1.0.py
+++ GoogleMeetBot_v1.0.py
@@ -1,7 +1,56 @@
-Things need to set up the bot
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+import time
 
-python3 v3.0.6 or above                                
-1>Selenium webdriver                                 
-python -m pip install selenium                            
-2>pyautoGui                                 
-python -m pip install pyautogui                         
+
+class ZoomBot:
+
+    def __init__(self, username, password, key1):
+        self.username = username
+        self.password = password
+        self.key1 = key1
+        options = webdriver.ChromeOptions()
+
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get('https://stackoverflow.com/users/login')
+        time.sleep(1)
+        google = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH,'//*[@id="openid-buttons"]/button[1]')))
+        google.click()
+        time.sleep(1)
+
+        email = WebDriverWait(self.driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@id='identifierId']")))
+        email.send_keys(username)
+        email.send_keys(Keys.ENTER)
+        time.sleep(5)
+        pyautogui.write(password, interval=0.05)
+        pyautogui.press('enter')
+        time.sleep(3)
+    def login(self):
+        driver = self.driver
+        try:
+            driver.get("https://meet.google.com")
+
+            keys = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#i2')))
+            keys.click()
+            keys.send_keys(key1)
+            keys.send_keys(Keys.ENTER)
+        except:
+            self.login()
+
+
+
+username = '' #Your Email Here
+password = '' #your Password Here
+key1 = '' # Your Key here
+try:
+    zoom1 = ZoomBot(username, password, key1)
+    zoom1.login()
+except:
+    zoom1 = ZoomBot(username, password, key1)
+    zoom1.login()
